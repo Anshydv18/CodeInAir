2f14763105cbdf4108c9e5a8051d0551b1d2a61c
Anshydv18 <anshgithub@gmail.com>
2024-09-10 17:49:16 +0530

    removed strict mode

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -34,10 +33,8 @@ const router = createBrowserRouter(
 );
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <AuthContextProvider>
     <RouterProvider router={router} />
     <Toaster />
     </AuthContextProvider>
-  </StrictMode>
 );
