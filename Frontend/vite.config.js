61675608f0534874ffc433385f81c758b77e5552
Anshydv18 <anshgithub@gmail.com>
2024-09-10 18:04:16 +0530

    Updated vercel.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Back-end server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite path if necessary
-      },
-    },
-  },
 });
